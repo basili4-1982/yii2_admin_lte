48355a68d86c564375d33115700f556d18c61fbb
Your Name <you@example.com>
2017-07-02 14:01:10 +0300

    ADMIN_LTE-2 Создать команду разворачивания приложения на github

---
+++ app/commands/DeployController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\commands;
+
+use yii\console\Controller;
+
+
+/**
+ *
+ * Команда публикации приложения на github.com
+ *
+ */
+class DeployController extends Controller
+{
+    public function actionIndex()
+    {
+        exec('git checkout master');
+        exec('git merge develop');
+        exec('git merge push');
+        exec('git merge push');
+        exec('git merge push --tags');
+        
+        return 0;
+    }
+}
